293b87aaa98af709ae1b4ad448f99b7eff16c41d
Nirant Kale <kalenirant@gmail.com>
2025-01-08 18:17:24 +0530

    Few more patterns and o/p

--- C-Lang/Programs/patterns.cpp
+++ C-Lang/Programs/patterns.cpp
@@ -88,8 +88,8 @@ void pattern9(int N){
     pattern8(N);
 }
 void pattern10(int N){
-    for(int i=1;i<2*N-1;i++){
-        int stars= (i<=N)? i : 2*N-i;
+    for(int i=0;i<2*N-1;i++){
+        int stars= (i<N)? i+1 : 2*N-i-1;
         for(int j=0;j<stars;j++){
             cout<<"*";
         }
@@ -203,16 +203,90 @@ void pattern19(int N) {
         cout << endl;
     }
 }
-// void pattern20(int N) {
-//     for (char i = 'A'+N-1; i >= 'A'; i--) {
-//         for (char j = i ; j <= 'A'+N-1 ; j++) {
-//             cout <<j;
-//         }
-//         cout << endl;
-//     }
-// }
+void pattern20(int N) {
+    for(int i=0; i < 2*N; i++) {
+        int spaces= (i<N)? 2*i:2*((2*N-i)-1) ;
+        int stars= (i<N)? (N-i): N-(2*N-i)+1;
+        // Stars
+            for(int j=0;j<stars ; j++) {
+                cout<<"*";
+            }
+        // Spaces
+            for(int j=0; j < spaces ; j++) {
+                cout<<" ";
+            }
+        // Stars
+            for(int j=0;j<stars ; j++) {
+                cout<<"*";
+            }
+        cout<<"\n";
+    }
+}
+void pattern21(int N){
+    for(int i=0;i<2*N-1;i++){
+        int stars= (i<N)? i+1 : 2*N-i-1;
+        int spaces= (i<N)? 2*(N-i)-2: 2*(i-N)+2;
+        // Stars
+        for(int j=0;j<stars;j++){
+            cout<<"*";
+        }
+        // Spaces
+        for(int j=0;j<spaces;j++){
+            cout<<" ";
+        }
+        // Stars
+        for(int j=0;j<stars;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+}
+void pattern22(int N){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+           if(i==0 || i==N-1 || j==0 || j==N-1){
+            cout<<"* ";
+           } 
+           else{
+            cout<<"  ";
+           }
+        }
+        cout<<"\n";
+    }
+}
+void pattern23(int N){
+    for(int i=0;i<2*N-1;i++){
+        for(int j=0;j<2*N-1;j++){
+         int top=i,left=j,right=(2*N-2)-j,bottom=(2*N-2)-i;
+            cout<<(N-min(min(left,right),min(top,bottom)))<<" ";
+        }
+        cout<<"\n";
+    }
+}
 int main(){
     int num=5;
+    pattern1(num);
+    pattern2(num);
+    pattern3(num);
+    pattern4(num);
+    pattern5(num);
+    pattern6(num);
+    pattern7(num);
+    pattern8(num);
+    pattern9(num);
+    pattern10(num);
+    pattern11(num);
+    pattern12(num);
+    pattern13(num);
+    pattern14(num);
+    pattern15(num);
+    pattern16(num);
+    pattern17(num);
+    pattern18(num);
     pattern19(num);
+    pattern20(num);
+    pattern21(num);
+    pattern22(num);
+    pattern23(num);
     return 0;
 }
